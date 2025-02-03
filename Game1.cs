d271532d4b11cb4f53871e6f295d5546a058b440
Sami-Jouamaa <samijouamaa@gmail.com>
2025-02-03 10:30:32 -0500

    basic code

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,6 +7,9 @@ namespace Dungeon_Crawler_Roguelite;
 
 public class Game1 : Game
 {
+    Texture2D ballTexture;
+    Vector2 ballPosition;
+    float ballSpeed;
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
@@ -19,6 +23,8 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        ballPosition = new Vector2(_graphics.PreferredBackBufferWidth/2, _graphics.PreferredBackBufferHeight/2);
+        ballSpeed = 100f;
 
         base.Initialize();
     }
@@ -27,6 +33,7 @@ public class Game1 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+        ballTexture = Content.Load<Texture2D>("ball");
         // TODO: use this.Content to load your game content here
     }
 
@@ -35,7 +42,27 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
+        float updatedBallSpeed = ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        
+        var keyboardState = Keyboard.GetState();
+
         // TODO: Add your update logic here
+        if (keyboardState.IsKeyDown(Keys.Up))
+        {
+            ballPosition.Y -= updatedBallSpeed;
+        }
+        if (keyboardState.IsKeyDown(Keys.Down))
+        {
+            ballPosition.Y += updatedBallSpeed;
+        }
+        if (keyboardState.IsKeyDown(Keys.Right))
+        {
+            ballPosition.X += updatedBallSpeed;
+        }
+        if (keyboardState.IsKeyDown(Keys.Left))
+        {
+            ballPosition.X -= updatedBallSpeed;
+        }
 
         base.Update(gameTime);
     }
@@ -45,6 +72,19 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(
+            ballTexture, 
+            ballPosition, 
+            null, 
+            Color.Red, 
+            0f, 
+            new Vector2(ballTexture.Width/2, ballTexture.Height/2),
+            Vector2.One, 
+            SpriteEffects.None, 
+            0f
+            );
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
